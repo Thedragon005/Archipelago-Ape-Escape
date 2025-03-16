283c3a21f299ba42c496bc025885addf88e703a4
Thedragon005 <149539303+Thedragon005@users.noreply.github.com>
2025-03-16 14:44:51 -0400

    Removed OOT imports

--- worlds/apeescape/Client.py
+++ worlds/apeescape/Client.py
@@ -5,7 +5,6 @@ import Utils
 from typing import TYPE_CHECKING, Optional, Dict, Set, ClassVar, Any, Tuple
 from Options import Toggle
 from NetUtils import ClientStatus
-from worlds.oot.Patches import get_override_table_bytes
 
 # TODO: REMOVE ASAP - Borrowed from MM2
 # This imports the bizhawk apworld if it's not already imported. This code block should be removed for a PR.
