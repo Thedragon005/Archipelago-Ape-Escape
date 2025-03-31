73d8475ba48742e558b262a6d87ea8b82e0e38c1
Thedragon005 <149539303+Thedragon005@users.noreply.github.com>
2025-03-31 10:05:38 -0400

    Removed Radar in Stadium Attack
    Removed it from SA because it could cause a bug

--- worlds/apeescape/Client.py
+++ worlds/apeescape/Client.py
@@ -986,15 +986,16 @@ class ApeEscapeClient(BizHawkClient):
         punch_Writes = []
 
         if gameState == RAM.gameState['InLevel']:
-            # If the current level is Gladiator Attack, the Sky Flyer is currently equipped, and the player does not have the Sky Flyer: unequip it
-            if ((currentLevel == 0x07)):
-                # Funny easter egg with Radar
-                if (gadgetStateFromServer & 4 != 0):
-                    gadgets_Writes += [(RAM.triangleGadgetAddress, 0x02.to_bytes(1, "little"), "MainRAM")]
+
+            #if ((currentLevel == 0x07)):
+                ## Funny easter egg with Radar
+                #if (gadgetStateFromServer & 4 != 0):
+                    #gadgets_Writes += [(RAM.triangleGadgetAddress, 0x02.to_bytes(1, "little"), "MainRAM")]
             if ((currentLevel == 0x0E)):
                 # Funny easter egg with Radar
                 if (gadgetStateFromServer & 4 != 0):
                     gadgets_Writes += [(RAM.triangleGadgetAddress, 0x02.to_bytes(1, "little"), "MainRAM")]
+                # If the current level is Gladiator Attack, the Sky Flyer is currently equipped, and the player does not have the Sky Flyer: unequip it
                 if (heldGadget == 6) and (gadgetStateFromServer & 64 == 0):
                     gadgets_Writes += [(RAM.crossGadgetAddress, 0xFF.to_bytes(1, "little"), "MainRAM")]
                     gadgets_Writes += [(RAM.heldGadgetAddress, 0xFF.to_bytes(1, "little"), "MainRAM")]
