59bfd5a20ebc218f6794f13f6123e45de149e6f1
jeffreybardon <jbardonjr@charter.net>
2025-04-03 20:17:03 -0400

    Correct region in Deveneom access rule

--- worlds/apeescape/Rules.py
+++ worlds/apeescape/Rules.py
@@ -2276,7 +2276,7 @@ def set_locations(self):
     else:
         connect_regions(self, AEDoor.CC_BASEMENT_ELEVATOR.value, AELocation.W7L3Deveneom.value, 
                         lambda state: HasWaterNet(state, self) or (HasNet(state, self) and (HasFlyer(state, self) or IJ(state, self) or CanSwim(state, self))))
-    connect_regions(self, AEDoor.CC_BASEMENT_ELEVATOR.value, AELocation.W7L3Deveneom.value, 
+    connect_regions(self, AEDoor.CC_BASEMENT_BUTTON_DOWN.value, AELocation.W7L3Deveneom.value, 
                         lambda state: (HasNet(state, self) or HasWaterNet(state, self)))
     # Button Room
     connect_regions(self, AEDoor.CC_BUTTON_BASEMENT_WATER.value, AELocation.W7L3Astur.value, 
