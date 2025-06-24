ce02a9fb70d61b4d16f326fe032f86c1bf03152b
Thedragon005 <149539303+Thedragon005@users.noreply.github.com>
2025-06-24 16:16:00 -0400

    Client version update (0.8.8)

--- worlds/apeescape/Client.py
+++ worlds/apeescape/Client.py
@@ -360,7 +360,7 @@ class ApeEscapeClient(BizHawkClient):
     system = "PSX"
 
     # TODO Remove when doing official PR
-    client_version = "0.8.7"
+    client_version = "0.8.8"
 
     local_checked_locations: Set[int]
     local_set_events: Dict[str, bool]
